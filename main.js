71808423f0b41b391a864ca901220d50bc983b4c
Christopher Joiner <cjoiner9656@gmail.com>
2026-04-21 19:28:16 -0400

    fixed errors in js code

--- main.js
+++ main.js
@@ -1,18 +1,50 @@
 const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".cities");
+
+const iconMap = {
+  "01d": "sun.svg",
+  "01n": "moon.svg",
+  "02d": "cloud-sun.svg",
+  "02n": "cloud-moon.svg",
+  "03d": "cloud.svg",
+  "03n": "cloud.svg",
+  "04d": "cloud.svg",
+  "04n": "cloud.svg",
+  "09d": "cloud-drizzle.svg",
+  "09n": "cloud-drizzle.svg",
+  "10d": "cloud-rain.svg",
+  "10n": "cloud-rain.svg",
+  "11d": "cloud-lightning.svg",
+  "11n": "cloud-lightning.svg",
+  "13d": "cloud-snow.svg",
+  "13n": "cloud-snow.svg",
+  "50d": "cloud-fog.svg",
+  "50n": "cloud-fog.svg",
+};
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const inputVal = input.value;
+  const inputVal = input.value.trim();
+  if (!inputVal) {
+    msg.textContent = "Please enter a city name";
+    return;
+  }
+
   const url = `/api/weather?q=${encodeURIComponent(inputVal)}`;
 
   fetch(url)
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("City not found");
+      }
+      return response.json();
+    })
     .then((data) => {
       const { main, name, sys, weather } = data;
-      const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+      const icon = `assets/weather-icons/${iconMap[weather[0]["icon"]] || "cloud.svg"}`;
       const li = document.createElement("li");
       li.classList.add("city");
       const markup = `
